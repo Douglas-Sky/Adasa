d62f78c13a81d641b553a32cda6ac3f52649ef22
Douglas Owusu Agyemang <douglas.owusuagyemang@kibo.school>
2024-06-10 09:40:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('btn-btn')
+btn.onClick = function() {
+    alert('Button has been clicked')
+}
+
+ let head = document.getElementById('btn-contact')
+btn.onClick = function() {
+    alert('Hello Tomorrow')
+}
+
+
+
+
